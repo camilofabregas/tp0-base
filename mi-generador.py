52e6ba3c92d5925a5fbd86273a6bebc4fd77f3b5
Camilo Fábregas <camilofabregas@outlook.com>
2025-08-30 20:10:44 -0300

    Remuevo log level
    Para intentar eliminar un error de tests en el ejercicio 2

--- mi-generador.py
+++ mi-generador.py
@@ -21,8 +21,7 @@ def main():
                 "image": "server:latest",
                 "entrypoint": "python3 /main.py",
                 "environment": [
-                    "PYTHONUNBUFFERED=1",
-                    "LOGGING_LEVEL=DEBUG"
+                    "PYTHONUNBUFFERED=1"
                 ],
                 "networks": ["testing_net"]
             }
@@ -46,8 +45,7 @@ def main():
             "image": "client:latest",
             "entrypoint": "/client",
             "environment": [
-                f"CLI_ID={i}",
-                "CLI_LOG_LEVEL=DEBUG"
+                f"CLI_ID={i}"
             ],
             "networks": ["testing_net"],
             "depends_on": ["server"]
