f76b196fb4768adb80518c7d6915597b64eea787
Camilo Fábregas <camilofabregas@outlook.com>
2025-08-31 12:22:11 -0300

    Create validar-echo-server.sh
    Probamos que el server funciona correctamente

---
+++ validar-echo-server.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PUERTO=$(awk -F' = ' '/SERVER_PORT/ {print $2}' server/config.ini | tr -d '[:space:]')
+
+# Mensaje de prueba
+TEST_MSG="hola-echo-server"
+
+RESULTADO=$(docker run --rm --network tp0_testing_net --entrypoint sh busybox -c "echo '$TEST_MSG' | nc server 5000")
+
+if [ "$RESULTADO" = "$TEST_MSG" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
