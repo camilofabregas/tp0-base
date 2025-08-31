7f701bca428cecb5d130c62ee3a223a09d2a80d5
Camilo Fábregas <camilofabregas@outlook.com>
2025-08-31 12:41:37 -0300

    Parametrizamos direccion server

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 PUERTO=$(awk -F' = ' '/SERVER_PORT/ {print $2}' server/config.ini | tr -d '[:space:]')
+ADDRESS=$(awk -F' = ' '/SERVER_IP/ {print $2}' server/config.ini | tr -d '[:space:]')
 
 # Mensaje de prueba
 TEST_MSG="hola-echo-server"
 
-RESULTADO=$(docker run --rm --network tp0_testing_net --entrypoint sh busybox -c "echo '$TEST_MSG' | nc server $PUERTO")
+RESULTADO=$(docker run --rm --network tp0_testing_net --entrypoint sh busybox -c "echo '$TEST_MSG' | nc $ADDRESS $PUERTO")
 
 if [ "$RESULTADO" = "$TEST_MSG" ]; then
     echo "action: test_echo_server | result: success"
