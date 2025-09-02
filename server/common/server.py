7d5a6f374cc8d8a48cd8d12ce87caeeaf92f6678
Camilo Fábregas <camilofabregas@outlook.com>
2025-09-02 18:17:00 -0300

    Graceful shutdown (server)
    Agregamos logica de graceful shutdown para el servidor utilizando signals

--- server/common/server.py
+++ server/common/server.py
@@ -1,6 +1,6 @@
 import socket
 import logging
-
+import signal
 
 class Server:
     def __init__(self, port, listen_backlog):
@@ -8,6 +8,16 @@ class Server:
         self._server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._server_socket.bind(('', port))
         self._server_socket.listen(listen_backlog)
+
+        # Set graceful shutdown flag
+        self._shutdown = False
+        signal.signal(signal.SIGTERM, self.__handle_sigterm)
+
+    def __handle_sigterm(self, signum, frame):
+        logging.info(f'action: server_shutdown | result: in_progress | signal: {signum}')
+        self._shutdown = True
+        self._server_socket.close()
+        logging.info("action: close_server_socket | result: success")
 
     def run(self):
         """
@@ -18,11 +28,15 @@ class Server:
         finishes, servers starts to accept new connections again
         """
 
-        # TODO: Modify this program to handle signal to graceful shutdown
-        # the server
-        while True:
-            client_sock = self.__accept_new_connection()
-            self.__handle_client_connection(client_sock)
+        while not self._shutdown:
+            try:
+                client_sock = self.__accept_new_connection()
+                self.__handle_client_connection(client_sock)
+            except OSError as e:
+                if not self._shutdown:
+                    logging.error(f'action: accept_connection | result: fail | error: {e}')
+
+        logging.info("action: server_shutdown | result: success")
 
     def __handle_client_connection(self, client_sock):
         """
