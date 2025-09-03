51a26744083cd029ddb842f8c81f36bd709b2fb3
Camilo Fábregas <camilofabregas@outlook.com>
2025-09-03 19:09:57 -0300

    Update client.go
    Fix log

--- client/common/client.go
+++ client/common/client.go
@@ -78,7 +78,7 @@ func (c *Client) StartClientLoop() {
 		// Create Bet from env
 		bet, err := from_env()
 		if err != nil {
-			log.Errorf("action: create_bet | result: fail | client_id: %v | msg: %v",
+			fmt.Printf("action: create_bet | result: fail | client_id: %v | msg: %v",
 				c.config.ID,
 				err,
 			)
