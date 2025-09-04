b1cfd53799a1e15ebce2a0f36980fd2f1f6dd00d
Camilo Fábregas <camilofabregas@outlook.com>
2025-09-04 09:41:31 -0300

    EOF Server
    Tratamos de handlear eof en el server

--- server/common/server.py
+++ server/common/server.py
@@ -44,6 +44,10 @@ class Server:
             len_bytes = self.__read_all(client_sock, 4)
             len_bytes = int.from_bytes(len_bytes, "big")
 
+            if len_bytes == 0:
+                logging.info("action: receive_message | result: eof | msg: empty batch received, closing connection")
+                return
+
             msg = self.__read_all(client_sock, len_bytes).rstrip().decode('utf-8')
             addr = client_sock.getpeername()
             logging.info(f'action: receive_message | result: success | ip: {addr[0]}')
